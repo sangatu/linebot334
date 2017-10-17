fc70c0de54df29de658031278d688e98620a24f7
naoto yamasaki <mad-hare@ezweb.ne.jp>
2017-10-17 11:56:09 +0900

    tesuto

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -276,6 +276,7 @@ public class KitchenSinkController {
                 }
                 break;
             }
+            
             case "訪問予約": {
                 ConfirmTemplate confirmTemplate = new ConfirmTemplate(
                 "Do it?",
