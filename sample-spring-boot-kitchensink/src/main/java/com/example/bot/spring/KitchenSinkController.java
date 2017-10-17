ca948aef52734773f6dfb0d43ba6e4cb8f0c8137
naoto yamasaki <mad-hare@ezweb.ne.jp>
2017-10-17 10:44:23 +0900

    てｓ

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -286,7 +286,7 @@ public class KitchenSinkController {
                 this.reply(replyToken, templateMessage);
                 break;
             }*/
-            case "1": {
+            case "あ: {
                 ConfirmTemplate confirmTemplate = new ConfirmTemplate(
                         "電話とネットのどちらで訪問予約を行いますか？",
                         new MessageAction("電話をかける", "電話で予約する"),
